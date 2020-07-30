4ee08ccb526b997672def127a10bb24f081f1f95
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-07-30 19:58:25 -0400

    skeleton example script

---
+++ example/roi_slicing.py
@@ -0,0 +1,14 @@
+import fire
+
+import pcatk
+
+def run():
+    """
+    pcatk.convert.ometif2tif
+    for img in folder.iterdir(): imsave(img[xl:xu, yl:yu])
+    pcatk.convert.tif2ometif
+    """
+
+
+if __name__ == "__main__":
+    fire.Fire(run)
